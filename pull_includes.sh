3b2208f4a5a1df0926f16ed41140f2b44d0f2bfc
Jake Wharton <jw@squareup.com>
2023-11-17 13:54:13 -0500

    Remove wayland/weston
    This was used in earlier experiments but we probably are not going to use it now.

--- pull_includes.sh
+++ pull_includes.sh
@@ -7,56 +7,17 @@ mkdir usr/downloads usr/include
 
 pushd usr/downloads > /dev/null
 
-echo "Downloading weston…"
+echo "Downloading mesa…"
 
-weston_version="8.0.0"
-curl -L --no-progress-meter "https://wayland.freedesktop.org/releases/weston-$weston_version.tar.xz" > "weston-$weston_version.tar.xz"
-tar -xzf "weston-$weston_version.tar.xz"
-rm "weston-$weston_version.tar.xz"
-rsync -a \
-  --include '*/' \
-  --include '*.h' \
-  --exclude '*' \
-  "weston-$weston_version/include/libweston" ../include
-rm -r "weston-$weston_version"
-
-echo "Downloading pixman…"
-
-pixman_version="0.34.0"
-curl -L --no-progress-meter "https://cairographics.org/releases/pixman-$pixman_version.tar.gz" > "pixman-$pixman_version.tar.gz"
-tar -xf "pixman-$pixman_version.tar.gz"
-rm "pixman-$pixman_version.tar.gz"
-cp "pixman-$pixman_version/pixman/pixman.h" ../include
-cp "pixman-$pixman_version/pixman/pixman-version.h" ../include
-rm -r "pixman-$pixman_version"
-
-echo "Downloading xkbcommon…"
-
-xkbcommon_version="0.7.0"
-curl -L --no-progress-meter "http://xkbcommon.org/download/libxkbcommon-$xkbcommon_version.tar.xz" > "libxkbcommon-$xkbcommon_version.tar.xz"
-tar -xf "libxkbcommon-$xkbcommon_version.tar.xz"
-rm "libxkbcommon-$xkbcommon_version.tar.xz"
-cp -r "libxkbcommon-$xkbcommon_version/xkbcommon" ../include
-rm -r "libxkbcommon-$xkbcommon_version"
-
-echo "Downloading libinput…"
-
-libinput_version="1.8.2"
-curl -L --no-progress-meter "https://www.freedesktop.org/software/libinput/libinput-$libinput_version.tar.xz" > "libinput-$libinput_version.tar.xz"
-tar -xf "libinput-$libinput_version.tar.xz"
-rm "libinput-$libinput_version.tar.xz"
-cp "libinput-$libinput_version/include/linux/input.h" ../include
-rm -r "libinput-$libinput_version"
+mesa_version="18.0.0"
+curl -L --no-progress-meter "https://archive.mesa3d.org/mesa-$mesa_version.tar.gz" > "mesa-$mesa_version.tar.xz"
+tar -xf "mesa-$mesa_version.tar.xz"
+rm "mesa-$mesa_version.tar.xz"
+cp "mesa-$mesa_version/src/gbm/main/gbm.h" ../include
+rm -r "mesa-$mesa_version"
 
 popd > /dev/null
 rmdir usr/downloads
-
-echo "Downloading wayland…"
-
-wayland_image=$(podman build -qf usr/Dockerfile.wayland)
-wayland_container=$(podman run -d $wayland_image)
-podman cp $wayland_container:/usr/local/include usr
-podman rm $wayland_container > /dev/null
 
 echo "Downloading libdrm…"
 
--- pull_libs.sh
+++ pull_libs.sh
@@ -6,5 +6,4 @@ rm -rf usr/lib
 mkdir usr/lib
 adb pull \
 	/usr/lib/libdrm.so \
-	/usr/lib/libwayland-client.so \
 	usr/lib/
