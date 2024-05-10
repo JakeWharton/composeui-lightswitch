bd4c76204483119a606def6902967d3653a8ee73
Jake Wharton <jw@squareup.com>
2024-05-09 22:30:35 -0400

    Only push if changed

--- run_release.sh
+++ run_release.sh
@@ -3,6 +3,16 @@
 set -e
 
 ./gradlew -q linkReleaseExecutableLinuxArm64
-adb push build/bin/linuxArm64/releaseExecutable/composeui-lightswitch.kexe /userdata
+
+local_md5="$(md5sum build/bin/linuxArm64/releaseExecutable/composeui-lightswitch.kexe | cut -d" " -f1 | tr -d '[:space:]')"
+remote_md5="$(adb shell 'md5sum /userdata/composeui-lightswitch.kexe | cut -d" " -f1' | tr -d '[:space:]')"
+
+echo "Local: '$local_md5'"
+echo "Remote: '$remote_md5'"
+
+if [[ "$local_md5" != "$remote_md5" ]]; then
+	adb push build/bin/linuxArm64/releaseExecutable/composeui-lightswitch.kexe /userdata
+fi
+
 #adb shell /userdata/composeui-lightswitch.kexe
 adb shell 'killall mixpad_gui && /userdata/composeui-lightswitch.kexe'
